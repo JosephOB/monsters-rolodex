bc7131cde1c537a0ea59215475080186688149c1
joseiboateng <joseiboateng@gmail.com>
2020-04-08 01:06:46 -0400

    Updates

---
+++ precache-manifest.27077669a96fce32a821d34adbde0434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4879cbfacd79c429d78d0dc3ba979392",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "65c65af83e20dcf2c39f",
+    "url": "/monsters-rolodex/static/css/main.00cb2bc5.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c65af83e20dcf2c39f",
+    "url": "/monsters-rolodex/static/js/main.680f925f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
